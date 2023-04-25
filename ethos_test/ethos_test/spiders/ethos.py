869072cd3516782664afdc7e11c1a883574b7010
erickfernandes_ <erickfernandesgon@gmail.com>
2023-04-25 15:21:29 -0300

    New websites has been added

--- ethos_test/ethos_test/spiders/ethos.py
+++ ethos_test/ethos_test/spiders/ethos.py
@@ -6,7 +6,7 @@ class KeywordsSpider(scrapy.Spider):
     headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
     }
-    start_urls = ['https://www.ethos.org.br/categoria/noticias/', 'https://www.pactoglobal.org.br/noticias', 'https://ibram.org.br/noticias/', 'https://www.gov.br/mma/pt-br/assuntos/noticias', 'https://www7.fiemg.com.br/Noticias', 'https://www.amcham.com.br/noticias']
+    start_urls = ['https://www.ethos.org.br/categoria/noticias/', 'https://www.pactoglobal.org.br/noticias', 'https://ibram.org.br/noticias/', 'https://www.gov.br/mma/pt-br/assuntos/noticias', 'https://www7.fiemg.com.br/Noticias', 'https://www.amcham.com.br/noticias','https://www.aberje.com.br/category/noticias/', 'https://abag.com.br/noticias-abag/']
 
     def parse(self, response):
         if 'https://www.ethos.org.br/categoria/noticias/' in response.url:
@@ -45,6 +45,17 @@ class KeywordsSpider(scrapy.Spider):
         for each_headline in response.css('.cover-collection-tile.tile-content'):
             yield {
                 'headline_news':str(each_headline.css('.cover-collection-tile.tile-content ::text').extract()).strip().replace('\n', "").replace('\\n', "").replace('               ', "")
-            }
 
+            }
+        for news_associations in response.css('.coluna9'):
+            yield {
+                'headline_aberge': str(
+                    news_associations.css('.coluna9 ::text').extract()).strip().replace(r'\n\t\t\t\t\t', "").replace(r'\t\t\t\t',"").replace(r'Página', ""),
+                'links_aberge': str(news_associations.css('.coluna9 ::attr(href)').extract())
+            }
+        for abag_association in response.css('.dg-blog-grid'):
+            yield {
+                'headline_abag': str(abag_association.css('.dg-blog-grid ::text').extract()),
+                'links_abag': str(abag_association.css('.dg-blog-grid ::attr(href)').extract())
+            }
 
